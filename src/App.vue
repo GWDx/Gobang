f5348104a34ebb99c2bf49530ff9233ae9e78a11
GWDx <gwdx@mail.ustc.edu.cn>
2022-03-10 21:10:22 +0800

    放置棋子

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
+<script>
+    let board = Array(36).fill('')
+    let color = 'black'
+    const inverseColor = { white: 'black', black: 'white' }
+
+    export default {
+        data() {
+            return { board: board, color: color }
+        },
+        methods: {
+            clickChessman(i) {
+                console.log('click ' + i)
+                if (board[i] !== '') return
+                board[i] = color
+                color = inverseColor[color]
+
+                console.log(board)
+                this.board = board.slice()
+                this.color = color
+            }
+        }
+    }
+</script>
+
 <template>
     <div class="box">
         <div class="squareGrid">
-            <div class="square" v-for="(x, i) in Array(25)" :key="i"></div>
+            <div class="square" v-for="(x, j) in Array(25)" :key="j"></div>
         </div>
 
         <div class="chessmanGrid">
             <div class="chessmanSquare" v-for="(x, i) in Array(36)" :key="i">
-                <div class="chessman"></div>
+                <div class="chessmanClickArea" @click="clickChessman(i)">
+                    <div class="chessman" v-if="board[i]" :style="{ background: board[i] }"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -50,14 +76,19 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        /* border: solid 1px gray; */
     }
-    .chessman {
+    .chessmanClickArea {
         width: 40%;
         height: 40%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .chessman {
+        width: 100%;
+        height: 100%;
         border-radius: 50%;
         border: solid 3px gray;
-        background: white;
         display: flex;
         align-items: center;
         justify-content: center;
